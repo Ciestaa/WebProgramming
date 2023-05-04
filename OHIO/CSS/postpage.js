eae37909afcdcbff5cf4f796206f379c0e66c1a5
Elyas Asmad <elyasasmadz@gmail.com>
2023-05-04 15:32:08 +0800

    haziq

--- OHIO/CSS/postpage.js
+++ OHIO/CSS/postpage.js
@@ -1,4 +1,38 @@
-document.getElementById("likeBtn").addEventListener("click", function() {
-    alert("Like button clicked");
+document.addEventListener('DOMContentLoaded', function() {
+  // Get all the stars
+  const stars = document.querySelectorAll('.rating .star');
+
+  // Add event listeners for hover and click
+  stars.forEach((star) => {
+    star.addEventListener('mouseover', () => {
+      // Highlight the stars up to this one
+      const rating = star.getAttribute('data-rating');
+      for (let i = 0; i < rating; i++) {
+        stars[i].classList.add('active');
+      }
+    });
+
+    star.addEventListener('mouseout', () => {
+      // Remove the highlighting
+      stars.forEach((s) => {
+        s.classList.remove('active');
+      });
+    });
+
+    star.addEventListener('click', () => {
+      // Set the rating to this star's value
+      const rating = star.getAttribute('data-rating');
+      alert(`You rated this ${rating} stars.`);
+    });
   });
-  
+});
+
+// Get the like button element
+const likeBtn = document.querySelector('.btn-like');
+
+// Add a click event listener to the like button
+likeBtn.addEventListener('click', function() {
+  // Toggle the 'liked' class to change the heart icon color
+  this.classList.toggle('liked');
+});
+
