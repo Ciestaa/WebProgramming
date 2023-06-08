6fc5fe4f1db0b1a2a29e1d47e6f16856d7685df3
Elyas Asmad <u2102723@siswa.um.edu.my>
2023-06-09 04:13:24 +0800

    Load more post update

---
+++ OHIO/load_more_posts.php
@@ -0,0 +1,55 @@
+<?php
+$conn = new mysqli('localhost', 'root', '', 'gotravel');
+if ($conn->connect_error) {
+    die('Connection failed: ' . $conn->connect_error);
+}
+
+if (isset($_POST['offset']) && isset($_POST['limit'])) {
+    $offset = $_POST['offset'];
+    $limit = $_POST['limit'];
+
+    // Fetch post details with offset and limit
+    $sql = "SELECT * FROM posts LIMIT $offset, $limit";
+    $result = $conn->query($sql);
+
+    function limitWords($string, $wordLimit) {
+        $words = explode(' ', $string);
+        if (count($words) > $wordLimit) {
+            $words = array_slice($words, 0, $wordLimit);
+            $string = implode(' ', $words) . '...';
+        }
+        return $string;
+    }   
+    
+
+    if ($result->num_rows > 0) {
+        // Loop through the rows and access each post detail
+        while ($row = $result->fetch_assoc()) {
+            $no = $row['No'];
+            $usernamePOST = $row['Username'];
+            $title = $row['Title'];
+            $description = $row['Description'];
+            $image = $row['Image'];
+            $location = $row['Location'];
+
+            // Display the post details here
+            echo '<div class="post-container">';
+            echo '<div class="post-content" style="display: flex; flex-direction: row; justify-content: space-between; align-items: center;">';
+            echo '<h2>' . $title . '</h2>';
+            echo '<button class="btn btn-like">';
+            echo '<i class="far fa-heart"></i>';
+            echo '</button>';
+            echo '</div>';
+            echo '<p style="text-align: justify;">' . limitWords($description, 100) . '</p>';
+            echo '<img src="' . $image . '" alt="Post Image" style="max-width: 100%; height: 150px; margin: 10px 0;">';
+            echo '<br>';
+            echo '<a style="color:black" href="../OHIO/viewpost_user.html">View blog</a>';
+            echo '</div>';
+        }
+    } else {
+        echo '<script>alert("No more posts to load.");</script>';
+    }
+}
+
+$conn
+?>
--- OHIO/post_page_user.php
+++ OHIO/post_page_user.php
@@ -19,34 +19,17 @@ if(isset($_POST["logout"])){
   exit;
 }
 
-require_once "config.php"; // Include the database connection configuration
 
-// Fetch all posts from the database
-$sql = "SELECT * FROM posts";
-$result = $mysqli->query($sql);
 
-// Check if any posts are found
-if ($result->num_rows > 0) {
-    // Iterate over the result set and display each post
-    while ($row = $result->fetch_assoc()) {
-        $postUsername = $row['Username'];
-        $postTitle = $row['Title'];
-        $postDescription = $row['Description'];
-        $postImage = $row['Image'];
-        $postLocation = $row['Location'];
 
-    }
-} else {
-    $post_err = "No posts found.";
-}
-
-$mysqli->close(); // Close the database connection
 
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
+
+
     <title>Post Page</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -56,16 +39,30 @@ $mysqli->close(); // Close the database connection
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.2/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
     <script>
       function search() {
-          var query = document.getElementById("search-bar").value;
-          if (query !== "") {
-              window.location.href = "../OHIO/searchUser.html?query=" + encodeURIComponent(query);
-          }
-      }
+            var query = document.getElementById("search-bar").value;
+            if (query !== "") {
+                window.location.href = "../OHIO/searchUser.html?query=" + encodeURIComponent(query);
+            }
+        }
+
+        function loadMorePosts() {
+            var offset = 0; // Initial offset value
+            var limit = 10; // Number of posts to load
+
+            // Call the fetchPosts function with the updated offset and limit values
+            fetchPosts(offset, limit);
+
+            // Update the offset for the next set of posts
+            offset += limit;
+        }
     </script>
 
 </head>
+
 <body>
     <div class="navbar">
         <div style="padding-left: 3%;"class="logo-container">
@@ -76,9 +73,8 @@ $mysqli->close(); // Close the database connection
           <form onsubmit="search(); return false;">
             <label for="search">Search:</label>
             <input type="text" id="search-bar" placeholder="Search for trips, user, or blog">
-        </form>
-      </div>
-      <div id="dynamic-content"></div>
+          </form>
+        </div>
         <div class="nav-links">
           <i class="bi bi-house-door text-white"></i>
           <a style="font-weight: bold; color:white; font-size:small;" href="../OHIO/post_page_user.php">HOME</a>
@@ -92,59 +88,85 @@ $mysqli->close(); // Close the database connection
             <input type="hidden" name="logout" value="1">
           </form>
         </div>
-        
     </div>
 
     <div class="post-section">
-        <div class="post-container">
-            <div class="post-content" style="display: flex; flex-direction: row; justify-content: space-between; align-items: center;">
-              <h2>Trip to Bangkok</h2>
-              <button class="btn btn-like">
-                <i class="far fa-heart"></i>
-              </button>
-            </div>
-            <p>Road here bengkang-bangkok</p>
-            <p style="text-align: justify;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu mauris at risus aliquet semper. Sed euismod, magna eget dapibus lobortis, velit eros semper est, nec venenatis magna mauris at lectus. Vestibulum quis vulputate purus. Donec pharetra dolor ut arcu euismod consectetur.</p>
-            <img src="CSS/Images/bangkok.jpg" alt="Post Image" style="max-width: 100%; height: 150px; margin: 10px 0;">
-            <img src="CSS/Images/bangkok.jpg" alt="Post Image" style="max-width: 100%; height: 150px; margin: 10px 0;">
-            <img src="CSS/Images/bangkok.jpg" alt="Post Image" style="max-width: 100%; height: 150px; margin: 10px 0;">
-            <br>
-            <a style="color:black" href="../OHIO/viewpost_user.html">View blog</a>
-          </div>
-          
-        <div class="post-container">
-            <div class="post-content" style="display: flex; flex-direction: row; justify-content: space-between; align-items: center;">
-              <h2>Trip to Bangkok</h2>
-              <button class="btn btn-like">
-                <i class="far fa-heart"></i>
-              </button>
-            </div>
-            <p>Road here bengkang-bangkok</p>
-            <p style="text-align: justify;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu mauris at risus aliquet semper. Sed euismod, magna eget dapibus lobortis, velit eros semper est, nec venenatis magna mauris at lectus. Vestibulum quis vulputate purus. Donec pharetra dolor ut arcu euismod consectetur.</p>
-            <img src="CSS/Images/bangkok.jpg" alt="Post Image" style="max-width: 100%; height: 150px; margin: 10px 0;">
-            <img src="CSS/Images/bangkok.jpg" alt="Post Image" style="max-width: 100%; height: 150px; margin: 10px 0;">
-            <img src="CSS/Images/bangkok.jpg" alt="Post Image" style="max-width: 100%; height: 150px; margin: 10px 0;">
-            <br>
-            <a style="color:black" href="../OHIO/viewpost_user.html">View blog</a>
-          </div>  
-    </div>
-        
-    <script>
-    // Get a reference to the dynamic content element
-    const dynamicContent = document.getElementById('dynamic-content');
+    <div id="post-container">
+        <?php
+      $conn = new mysqli('localhost', 'root', '', 'gotravel');
+      if ($conn->connect_error) {
+        die('Connection failed: ' . $conn->connect_error);
+      }
 
-    // Fetch data from an API (replace 'api-url' with your actual API endpoint)
-    fetch('api-url')
-      .then(response => response.json())
-      .then(data => {
-        // Update the dynamic content with the fetched data
-        dynamicContent.textContent = JSON.stringify(data);
-      })
-      .catch(error => {
-        // Handle any errors that occurred during the fetch
-        console.error('Error:', error);
-      });
-  </script>
+      function fetchPosts($conn, $offset, $limit) {
+        // Fetch post details with offset and limit
+        $sql = "SELECT * FROM posts LIMIT $offset, $limit";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+          // Loop through the rows and access each post detail
+          while ($row = $result->fetch_assoc()) {
+            $no = $row['No'];
+            $usernamePOST = $row['Username'];
+            $title = $row['Title'];
+            $description = $row['Description'];
+            $image = $row['Image'];
+            $location = $row['Location'];
+
+            // Display the post details here
+            echo '<div class="post-container">';
+            echo '<div class="post-content" style="display: flex; flex-direction: row; justify-content: space-between; align-items: center;">';
+            echo '<h2>' . $title . '</h2>';
+            echo '<button class="btn btn-like">';
+            echo '<i class="far fa-heart"></i>';
+            echo '</button>';
+            echo '</div>';
+            echo '<p style="text-align: justify;">' . $description . '</p>';
+            echo '<img src="' . $image . '" alt="Post Image" style="max-width: 100%; height: 150px; margin: 10px 0;">';
+            echo '<br>';
+            echo '<a style="color:black" href="../OHIO/viewpost_user.html">View blog</a>';
+            echo '</div>';
+          }
+        } else {
+          echo "No posts found.";
+        }
+      }
+
+      // Fetch the first 10 post details
+      fetchPosts($conn, 0, 10);
+      ?>
+    </div>
+    <center>
+        <button type="button" class="btn btn-success" onclick="loadMorePosts()">Load More</button>
+    </center>
+    <div class="loader-div" style="display: none;">Loading...</div>
+</div>
+
+<script type="text/javascript">
+    function loadMorePosts() {
+        var offset = document.getElementsByClassName('post-container').length; // Get the number of existing posts
+        var limit = 10; // Number of posts to load per request
+
+        // Make an AJAX request to load more posts
+        $.ajax({
+            url: 'load_more_posts.php', // Path to the PHP script handling the request
+            type: 'POST',
+            data: {
+                offset: offset,
+                limit: limit
+            },
+            dataType: 'html',
+            success: function(response) {
+                // Append the loaded posts to the existing post container
+                $('#post-container').append(response);
+            },
+            error: function(xhr, status, error) {
+                console.error(error);
+            }
+        });
+    }
+</script>
+       
 </body>
 
   
