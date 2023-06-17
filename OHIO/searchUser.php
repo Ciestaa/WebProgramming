4d7d0cf97730c0a70e1cfb0d804659bcf04aa425
SirXise <ninjadororo25@gmail.com>
2023-06-17 21:25:49 +0800

    hfgfgh

--- OHIO/searchUser.php
+++ OHIO/searchUser.php
@@ -14,7 +14,7 @@
         function search() {
             var query = document.getElementById("search-bar").value;
             if (query !== "") {
-                window.location.href = "../OHIO/searchUser.html?query=" + encodeURIComponent(query);
+                window.location.href = "../OHIO/searchUser.php?query=" + encodeURIComponent(query);
             }
         }
     </script>
@@ -49,58 +49,102 @@
   </div>
 <div class="container">
 <?php
-      $conn = new mysqli('localhost', 'root', '', 'gotravel');
-      if ($conn->connect_error) {
-        die('Connection failed: ' . $conn->connect_error);
+// Retrieve the search query from the URL parameter
+$query = isset($_GET['query']) ? $_GET['query'] : '';
+
+// Perform the search logic based on the query
+if (!empty($query)) {
+  // Perform your search logic here using the $query variable
+  // Example: Query the database to search for users matching the query
+  $conn = new mysqli('localhost', 'root', '', 'gotravel');
+  if ($conn->connect_error) {
+    die('Connection failed: ' . $conn->connect_error);
+  }
+
+  // Search for users
+  $userSql = "SELECT * FROM userdetail WHERE Username LIKE '%$query%'";
+  $userResult = $conn->query($userSql);
+
+  // Search for posts
+  $postSql = "SELECT * FROM posts WHERE Title LIKE '%$query%' OR Location LIKE '%$query%'";
+  $postResult = $conn->query($postSql);
+
+  // Display user search results
+  if ($userResult->num_rows > 0) {
+    echo '<h2>User Results:</h2>';
+
+    while ($userRow = $userResult->fetch_assoc()) {
+      $username = $userRow['Username'];
+      $fullName = $userRow['FullName'];
+      $gender = $userRow['Gender'];
+      $phoneNo = $userRow['PhoneNo'];
+      $instagram = $userRow['Instagram'];
+      $yearTravel = $userRow['YearTravel'];
+      $countryTravel = $userRow['CountryTravel'];
+      $profilePic = $userRow['ProfilePic'];
+
+      // Display the user details here
+      echo '<div class="white-box">';
+      echo '<h3>' . $fullName . '</h3>';
+      echo '<p>Username: ' . $username . '</p>';
+      echo '<p>Gender: ' . $gender . '</p>';
+      echo '<p>Phone No: ' . $phoneNo . '</p>';
+      echo '<p>Instagram: ' . $instagram . '</p>';
+      echo '<p>Year of Travel: ' . $yearTravel . '</p>';
+      echo '<p>Country of Travel: ' . $countryTravel . '</p>';
+      echo '</div>';
+      echo '<div class="box-image">';
+      if ($profilePic != null) {
+        echo '<img src="' . $profilePic . '" alt="Post Image" style="max-width: 100%; height: 150px; margin: 10px 0;">'; 
+        echo '<br>';
       }
+      echo '</div>';
+      echo '</a>';
+      
+    }
+  } else {
+    echo '<h2>No user results found.</h2>';
+  }
 
-      function fetchPosts($conn) {
-        // Fetch post details with offset and limit
-        $sql = "SELECT * FROM posts ORDER BY postID DESC";
-        $result = $conn->query($sql);
+  // Display post search results
+  if ($postResult->num_rows > 0) {
+    echo '<h2>Post Results:</h2>';
 
-        if ($result->num_rows > 0) {
-          // Loop through the rows and access each post detail
-          while ($row = $result->fetch_assoc()) {
-            $PostID = $row['postID'];
-            $usernamePOST = $row['Username'];
-            $title = $row['Title'];
-            $description = $row['Description'];
-            $image = $row['Image'];
-            $location = $row['Location'];
+    while ($postRow = $postResult->fetch_assoc()) {
+      $postID = $postRow['postID'];
+      $usernamePOST = $postRow['Username'];
+      $title = $postRow['Title'];
+      $description = $postRow['Description'];
+      $image = $postRow['Image'];
+      $location = $postRow['Location'];
 
-            // Display the post details here
-            
-              echo'<a href="../OHIO/viewpost_user.php?post_id=' . $PostID . '" class="white-box">';
-                echo'<div class="box-text">';
-                  echo'<h2>'.$title.'</h2>';
-                  echo'<p>' . truncateText($description, 100) . '</p>';
-                echo'</div>';
-                echo'<div class="box-image">';
-                  if($image != null)
-                    { 
-                      echo '<img src="' . $image . '" alt="Post Image" style="max-width: 100%; height: 150px; margin: 10px 0;">';
-                      echo '<br>';
-                    }
-                echo'</div>';
-              echo'</a>';
-            
-          }
-        } else {
-          echo "No posts found.";
-        }
+      // Display the post details here
+      echo '<a href="../OHIO/viewpost_user.php?post_id=' . $postID . '" class="white-box">';
+      echo '<div class="box-text">';
+      echo '<h2>' . $title . '</h2>';
+      echo '<p>' . truncateText($description, 100) . '</p>';
+      echo '</div>';
+      echo '<div class="box-image">';
+      if ($image != null) {
+        echo '<img src="' . $image . '" alt="Post Image" style="max-width: 100%; height: 150px; margin: 10px 0;">'; 
+        echo '<br>';
       }
-      function truncateText($text, $limit) {
-        if (strlen($text) > $limit) {
-            $truncated = substr($text, 0, $limit);
-            return $truncated . '...';
-        }
-        return $text;
-      }
+      echo '</div>';
+      echo '</a>';
+    }
+  } else {
+    echo '<h2>No post results found.</h2>';
+  }
+}
 
-      // Fetch the first 10 post details
-      fetchPosts($conn);
-      ?>
+function truncateText($text, $limit) {
+  if (strlen($text) > $limit) {
+    $truncated = substr($text, 0, $limit);
+    return $truncated . '...';
+  }
+  return $text;
+}
+?>
 </div>
 </body>
 
